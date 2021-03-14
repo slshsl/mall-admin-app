dcdd447cab18f7e5e16fd416098f86bb5a995b1b
ShiLei <gyy137145@163.com>
2021-03-14 22:26:57 +0800

    优化

---
+++ src/views/layout/components/sliderNav.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="main-header">
+    <a-button
+      type="primary"
+      style="margin-bottom: 16px"
+      @click="toggleCollapsed"
+    >
+      <a-icon :type="$store.state.collapsed ? 'menu-unfold' : 'menu-fold'" />
+    </a-button>
+    <div class="breadcrump">
+      <a-breadcrumb>
+        <a-breadcrumb-item>Home</a-breadcrumb-item>
+        <a-breadcrumb-item><a href="">Application Center</a></a-breadcrumb-item>
+      </a-breadcrumb>
+    </div>
+    <ul class="user-infor">
+      <li>
+        欢迎
+        <a-icon type="down" />
+      </li>
+      <li>退出</li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  methods: {
+    toggleCollapsed() {
+      this.$store.dispatch('changeCollapsed');
+    },
+  },
+};
+</script>
